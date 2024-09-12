3ff6b1211d92b62a23321f7be59e2d324c3c5b27
aayush <aayushlad058@gmail.com>
2024-09-12 22:55:46 +0530

    only one batch process queue is working due to ec2

--- submissionWorker/src/index.ts
+++ submissionWorker/src/index.ts
@@ -1,49 +1,29 @@
 import express from "express";
-import cluster from "cluster";
 import { connectRedis } from "./database/redisClient";
-import { taskProcessQueue } from "./workers/taskQueueProcessor";
 import { batchTaskQueueProcessor } from "./workers/batchTaskQueueProcessor";
+import { taskProcessQueue } from "./workers/taskQueueProcessor";
 
 const PORT = 3001;
 
-if (cluster.isPrimary) {
-	const numCPUs = 2; // Limit to 2 workers
-	console.log(`Primary ${process.pid} is running`);
+(async function () {
+	try {
+		await connectRedis();
+		console.log("Redis connected");
 
-	// Fork workers (2 workers only)
-	for (let i = 0; i < numCPUs; i++) {
-		cluster.fork();
+		const app = express();
+
+		// can not ruth this bcz EC2 has only 1 cpu
+
+		// await taskProcessQueue();
+		// console.log("Task Worker started");
+
+		await batchTaskQueueProcessor();
+		console.log("Batch Task Worker started");
+
+		app.listen(PORT, () => {
+			console.log(`Server handling requests on port ${PORT}`);
+		});
+	} catch (error) {
+		console.log("Error initializing server:", error);
 	}
-
-	// Restart worker on exit
-	cluster.on("exit", (worker, code, signal) => {
-		console.log(`Worker ${worker.process.pid} died. Restarting...`);
-		cluster.fork();
-	});
-} else {
-	// Worker processes
-	(async function () {
-		try {
-			await connectRedis();
-			console.log("Redis connected");
-
-			const app = express();
-
-			// Task assignment to worker 1 and worker 2
-			if (cluster.worker && cluster.worker.id === 1) {
-				await taskProcessQueue();
-				console.log("Task Process Worker started");
-			} else if (cluster.worker && cluster.worker.id === 2) {
-				await batchTaskQueueProcessor();
-				console.log("Batch Task Worker started");
-			}
-
-			// Each worker listens on the same port, managed by the primary process
-			app.listen(PORT, () => {
-				console.log(`Worker ${process.pid} handling requests on port ${PORT}`);
-			});
-		} catch (error) {
-			console.log("Error initializing worker:", error);
-		}
-	})();
-}
+})();
